d19ebadf1d38754dfbb5bae8e0e0000cb9dc57b4
unknown <emmadan073@gmail.com>
2025-07-04 10:57:27 +0100

    TF-179 [Health Parser] Update UI to Support Multiple Report Entries with Separate Names

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -16,7 +16,7 @@ function Input({
   const sharedClasses = cn(
     "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
     "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-    "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive","h-[300]",
+    "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive","h-[100]",
     className
   );
 
